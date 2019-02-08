7012e0656ff5a7be723f23030424681c01429229
tfranczak <tfranczak@anl.gov>
2019-02-07 19:50:13 -0600

    Added a mysql connection singleton module

---
+++ connection.js
@@ -0,0 +1,31 @@
+// This module establishes a singleton database object for querying
+
+const mysql = require('mysql');
+var connection;
+
+function connectDatabase() {
+	// Create the connection if it does not already exist.
+	if (!connection) {
+		connection = mysql.createConnection({
+			host     : process.env.RDS_HOSTNAME,
+			user     : process.env.RDS_USERNAME,
+			password : process.env.RDS_PASSWORD,
+			port     : process.env.RDS_PORT,
+			database : process.env.RDS_DBNAME,
+			multipleStatements: true
+		});
+		
+		connection.connect((err) => {
+			if (err) {
+				console.log('Database failed to connect.');
+			}
+			else {
+				console.log('Database connection successful.');
+			}
+		});
+	}
+	
+	return connection;
+}
+
+module.exports = connectDatabase();
