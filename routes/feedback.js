5203ecdc7f9a05fe29d894f8f600831e9b5ec0e2
tommyfranczak <thomasfranczak@discover.com>
2019-02-09 15:01:05 -0600

    Added listening message, multiple feedback queries

--- routes/feedback.js
+++ routes/feedback.js
@@ -2,14 +2,18 @@ const express = require('express');
 const router = express.Router();
 var connection = require('../connection');
 
+
 router.post('/feedback/createTable', (req, res) => {
 	/*
-	var sql = 'CREATE TABLE IF NOT EXISTS valkyriePrimaryDB.feedback(' +
+	let sql = 'CREATE TABLE IF NOT EXISTS valkyriePrimaryDB.feedback(' +
 						'feedbackID INT AUTO_INCREMENT, ' +
 						'device_address_sender VARCHAR(40), ' +
 						'device_address_receiver VARCHAR(40), ' +
 						'feedback VARCHAR(240), ' +
-						'timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)'
+						'timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, ' +
+						'PRIMARY KEY (feedbackID), ' +
+						'FOREIGN KEY (device_address_sender) REFERENCES valkyriePrimaryDB.userinfotable(device_address), ' +
+						'FOREIGN KEY (device_address_receiver) REFERENCES valkyriePrimaryDB.userinfotable(device_address));
 	connection.query(sql, (error, results) => {
 		if(error) {
 			response.status(500).send({table_create_status: "Failed: " + error});
@@ -25,11 +29,18 @@ router.post('/feedback/createTable', (req, res) => {
 
 router.get('/feedback/received', (req, res) => {
 	/*
-	var receiver = req.body.deviceaddress;
+	let device_address_receiver = req.query.device_address;
+	if (!device_address_receiver) {
+		res.status(400).send({feedback_given_status : 'Failed: Missing device_address parameter'});
+	}
 
-	// Join device_address sender and receiver on firstname/lastname
-	var SQL = 'SELECT first_name, last_name, feedback, timestamp'
-	connection.query(SQL, (error, results) => {
+	// Join device_address receiver and receiver on firstname/lastname
+	let SQL = 'SELECT ui.first_name, ui.last_name, f.feedback, f.timestamp ' +
+			  'FROM valkyriePrimaryDB.userinfotable AS ui ' +
+			  'INNER JOIN valkyriePrimaryDB.feedback AS f ' +
+			  'ON ui.device_address = f.device_address_sender
+			  'WHERE ui.device_address = ?;';
+	connection.query(SQL, [device_address_sender],(error, results) => {
 		if (error) {
 			res.status(500).send({feedback_received_status : 'Failed:' + error})
 		}
@@ -44,12 +55,19 @@ router.get('/feedback/received', (req, res) => {
 
 
 router.get('/feedback/given', (req, res) => {
-		/*
-	var receiver = req.body.deviceaddress;
+	/*
+	let device_address_giver = req.body.device_address;
+	if (!device_address_giver) {
+		res.status(400).send({feedback_given_status : 'Failed: Missing device_address parameter'});
+	}
 
 	// Join device_address sender and receiver on firstname/lastname
-	var SQL = 'SELECT first_name, last_name, feedback, timestamp
-	connection.query(SQL, (error, results) => {
+	let SQL = 'SELECT ui.first_name, ui.last_name, f.feedback, f.timestamp ' +
+			  'FROM valkyriePrimaryDB.userinfotable AS ui ' +
+			  'INNER JOIN valkyriePrimaryDB.feedback AS f ' +
+			  'ON ui.device_address = f.device_address_receiver
+			  'WHERE ui.device_address = ?;';
+	connection.query(SQL, [device_address_giver], (error, results) => {
 		if (error) {
 			res.status(500).send({feedback_received_status : 'Failed:' + error})
 		}
@@ -62,9 +80,29 @@ router.get('/feedback/given', (req, res) => {
 	res.send("Given.");
 });
 
-router.post('/feedback/give', (req, res) => {
+router.post('/feedback/insert', (req, res) => {
+	/*
+	let device_address_giver = req.body.device_address_giver;
+	let device_address_receiver = req.body.device_address_receiver;
+	let feedback = req.body.feedback;
+	if (!device_address_giver || !device_address_receiver || !feedback) {
+		res.status(400).send({feedback_insert_status: 'Failed: Required arguments are device_address_giver, device_address_receiver, and feedback.'});
+	}
 
-	res.send("give")
+	// Join device_address sender and receiver on firstname/lastname
+	let SQL = 'INSERT INTO valkyriePrimaryDB.feedback (device_address_giver, device_address_receiver, feedback) ' +
+			  'VALUES ( ?, ?, ?);';
+	connection.query(SQL, [device_address_giver, device_address_receiver, feedback], (error, results) => {
+		if (error) {
+			res.status(500).send({feedback_received_status : 'Failed:' + error})
+		}
+		else {
+			res.status(200).send({feedback_received_status : 'Successful',
+							results : results});
+		}
+	}
+	*/
+	res.send("insert")
 });
 
 module.exports = router;
