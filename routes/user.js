02d532a39c5963a3dfd3593fe2b9c58f7b2c06e6
Thomas Franczak <tfrancz@discover.com>
2019-02-26 14:15:34 -0600

    Modified user.js

--- routes/user.js
+++ routes/user.js
@@ -785,7 +785,7 @@ function updateArray(id) {
     SQL = "SELECT skill, skill_level, skill_ID FROM skills WHERE device_address = ?;"
     connection.query(SQL, [id], (error, results) => {
       if (error) return false;
-      skills = results;
+      userinfo["skills"] = results;
 
       // Update entry in currentUsers array
       for (let i = 0;  i < currentUsers.length; i++) {
@@ -793,7 +793,7 @@ function updateArray(id) {
           currentUsers.splice(i, 1);  // Delete it
                                       // and reload it
           currentUsers.push(userinfo);
-          currentUsers[i]["skills"] = skills;
+          // currentUsers[i]["skills"] = skills;
 
           currentUsers.sort(compare);
           return true;
@@ -802,7 +802,6 @@ function updateArray(id) {
 
       // Create new entry in currentUsers array
       currentUsers.push(userinfo);
-      currentUsers[currentUsers.length-1]["skills"] = skills;
       currentUsers.sort(compare);
       return true;
     });
